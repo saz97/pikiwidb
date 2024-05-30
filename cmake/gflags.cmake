7e15c80266102b66870ebb9dff79b9d48c2ac7e8
panlei-coder <62509266+panlei-coder@users.noreply.github.com>
2024-05-30 13:03:50 +0800

    fix: fix a bug of not find gflags (#335)

--- cmake/gflags.cmake
+++ cmake/gflags.cmake
@@ -5,6 +5,9 @@
 
 INCLUDE_GUARD()
 
+SET(GFLAGS_BUILD_TYPE "Release")
+SET(CMAKE_BUILD_TYPE ${GFLAGS_BUILD_TYPE})
+
 FetchContent_Declare(gflags
   URL https://github.com/gflags/gflags/archive/v2.2.2.zip
   URL_HASH SHA256=19713a36c9f32b33df59d1c79b4958434cb005b5b47dc5400a7a4b078111d9b5
@@ -21,6 +24,8 @@ FIND_PACKAGE(Threads REQUIRED)
 
 TARGET_LINK_LIBRARIES(gflags_static Threads::Threads)
 
-SET(GFLAGS_INCLUDE_PATH ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/include)
-SET(GFLAGS_LIBRARY ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/libgflags.a)
-SET(GFLAGS_LIB ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/libgflags.a)
+SET(GFLAGS_INCLUDE_PATH ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/include CACHE PATH "" FORCE)
+SET(GFLAGS_LIBRARY ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/libgflags.a CACHE PATH "" FORCE)
+SET(GFLAGS_LIB ${CMAKE_CURRENT_BINARY_DIR}/_deps/gflags-build/libgflags.a CACHE PATH "" FORCE)
+
+SET(CMAKE_BUILD_TYPE ${THIRD_PARTY_BUILD_TYPE})
